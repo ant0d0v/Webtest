fae15f35038262b82f265cbbe35282be3b61e9c2
ant0d0v <gosu1203@gmail.com>
2023-01-19 20:13:12 +0200

    3 test

--- src/test/java/Web_test.java
+++ src/test/java/Web_test.java
@@ -81,22 +81,96 @@ public class Web_test {
             driver.get(url);
 
 
+            Thread.sleep(5000);
 
-        Thread.sleep(5000);
+            WebElement guidElementMenu = driver.findElement(
+                    By.xpath("//nav[@id = 'nav-website' ]//a[@href = '/guide']"));
 
-        WebElement guidElementMenu = driver.findElement(
-                By.xpath("//nav[@id = 'nav-website' ]//a[@href = '/guide']"));
+            guidElementMenu.click();
+            Thread.sleep(5000);
 
-        guidElementMenu.click();
-        Thread.sleep(5000);
+            String actualResultUrl = driver.getCurrentUrl();
+            String actualResultTitle = driver.getTitle();
 
-        String actualResultUrl = driver.getCurrentUrl();
-        String actualResultTitle = driver.getTitle();
+            Assert.assertEquals(actualResultTitle, expectedResultTitle);
+            Assert.assertEquals(actualResultUrl, expectedResultUrl);
 
-        Assert.assertEquals(actualResultTitle,expectedResultTitle);
-        Assert.assertEquals(actualResultUrl, expectedResultUrl);
+            driver.quit();
 
-        driver.quit();
+        }
+
+        // кейс 3
+        // 1 Открыть страницу https://openweathermap.org
+        // 2 Нажать на единицы измерения Imperial F
+        // 3 Потвердить что температура показана в фарингейтах
+
+
+
+
+
+        @Test
+        public void testTemperature() throws InterruptedException {
+
+            System.setProperty("webdriver.chrome.driver", "/Applications/chromedriver_mac_arm64/chromedriver");
+
+            WebDriver driver = new ChromeDriver();
+
+            String url = "https://openweathermap.org";
+            String expectedResultTemperature = "°F";
+            String fTempSymbols = "°F";
+
+            driver.get(url);
+            Thread.sleep(5000);
+
+            WebElement temperatureImperialF = driver.findElement(
+                    By.xpath("//div[@class = 'controls']//div[3]"));
+
+            temperatureImperialF.click();
+            Thread.sleep(5000);
+
+            WebElement tempF = driver.findElement(
+                    By.xpath("//div[@id = 'weather-widget']/div[2]/div[1]/div[1]/div[2]/div[1]/span"));
+
+            String actualResult = tempF.getText();
+
+            // actualResult.length()-2 - возрощает последние 2 символа
+            Assert.assertEquals(actualResult.substring(actualResult.length()-2), expectedResultTemperature);
+
+            driver.quit();
+        }
+
+            // кейс 4
+            // 1. Открыть страницу https://openweathermap.org
+            // 2. Потвердить что внизу страницы есть панель с текстом "We use cookies which are essential for
+            // the site to work. We also use non-essential cookies to help us improve our services.
+            // Any data collected is anonymised. You can allow all cookies or manage them individually."
+            // 3. Потвердить что на панели в низу ест 2 кнопки   Manage cookies  and Allow all
+
+
+            @Test
+            public void popupCookie() throws InterruptedException {
+
+                System.setProperty("webdriver.chrome.driver", "/Applications/chromedriver_mac_arm64/chromedriver");
+
+                WebDriver driver = new ChromeDriver();
+
+                String url = "https://openweathermap.org";
+                String textCookie = "We use cookies which are essential "
+                        + "for the site to work. We also use non-essential cookies to help us improve our services. "
+                        + "Any data collected is anonymised. You can allow all cookies or manage them individually.";
+                String fTempSymbols = "°F";
+
+                driver.get(url);
+                Thread.sleep(5000);
+
+                WebElement stickFooterPanel = driver.findElement(
+                        By.xpath("//div[@class = 'stick-footer-panel']"));
+
+
+
+
+
+
 
 
 
